3579ad27f5b6f8f58ca778709260a4e981ee0eac
Dylangroome <dylangroome13@gmail.com>
2023-03-31 11:18:15 +0000

    add first models

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,73 @@ from cloudinary.models import CloudinaryField
 
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Genre(models.Model):
+	title = models.CharField(max_length=25)
+	slug = models.SlugField(null=False, unique=True)
+
+	def __str__(self):
+		return self.title
+
+
+class Artist(models.Model):
+	name = models.CharField(max_length=70, unique=True)
+	featured_image = CloudinaryField('image', default='placeholder')
+	slug = models.SlugField(null=True, unique=True)
+	albums = models.ManyToManyField(Album, blank=True)
+
+	def __str__(self):
+		return self.name
+
+
+class Rating(models.Model):
+	source = models.CharField(max_length=50)
+	rating = models.CharField(max_length=10)
+
+	def __str__(self):
+		return self.source
+
+
+class Album(models.Model):
+	Title = models.CharField(max_length=200)
+	Year = models.CharField(max_length=25, blank=True)
+	Rated = models.CharField(max_length=10, blank=True)
+	Released = models.CharField(max_length=25, blank=True)
+	Genre = models.ManyToManyField(Genre, blank=True)
+	Artist = models.ManyToManyField(Artist, blank=True)
+	featured_image = CloudinaryField('image', default='placeholder')
+	Ratings = models.ManyToManyField(Rating, blank=True)
+
+	def __str__(self):
+		return self.Title
+
+
+RATE_CHOICES = [
+	(1, '1 - Burn it'),
+	(2, '2 - Mute'),
+	(3, '3 - Terrible'),
+	(4, '4 - Bad'),
+	(5, '5 - OK'),
+	(6, '6 - Listenable'),
+	(7, '7 - Good'),
+	(8, '8 - Recommended'),
+	(9, '9 - Perfect'),
+	(10, '10 - Master Piece'), 
+]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
