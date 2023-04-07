35c9ee6c28efcfe495d59925ae263111b6cd3181
Dylangroome <dylangroome13@gmail.com>
2023-04-07 19:11:44 +0000

    url paths for views

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.AlbumList.as_view(), name="home"),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post'),
+    path('about/', views.AboutTemplateView.as_view(), name='about'),
+    path('contact/', views.ContactTemplateView.as_view(), name='contact'),
+    path('post/', views.PostTemplateView.as_view(), name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.views.generic import DeleteView, UpdateView
+from django.views.generic.base import TemplateView
 from .models import Album
 
 
@@ -10,22 +14,43 @@ class AlbumList(generic.ListView):
     paginate_by = 6
 
 
-class PostDetail(View):
+class AboutTemplateView(TemplateView):
+    '''
+    For the about page url
+    '''
+    template_name = 'about.html'
 
-    def get(self, request, slug, *args, **kwargs):
-        queryset = Album.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
 
-        return render(
-            request,
-            "post.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked
-            },
-        )
+class ContactTemplateView(TemplateView):
+    '''
+    For the contact page url
+    '''
+    template_name = 'contact.html'
+
+
+class PostTemplateView(TemplateView):
+    '''
+    For the post page url
+    '''
+    template_name = 'post.html'
+
+
+# class PostDetail(View):
+
+#     def get(self, request, slug, *args, **kwargs):
+#         queryset = Album.objects.filter(status=1)
+#         post = get_object_or_404(queryset, slug=slug)
+#         comments = post.comments.filter(approved=True).order_by("-created_on")
+#         liked = False
+#         if post.likes.filter(id=self.request.user.id).exists():
+#             liked = True
+
+#         return render(
+#             request,
+#             "post.html",
+#             {
+#                 "post": post,
+#                 "comments": comments,
+#                 "liked": liked
+#             },
+#         )
